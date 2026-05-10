51c9553b1013ae654e59183c1d655c0421f9648b
Marcelo Castro <marcelomacielcastro@outlook.com>
2026-05-10 19:09:09 -0300

    troubleshooting render migrations

--- BibliotecaPI/BibliotecaPI/urls.py
+++ BibliotecaPI/BibliotecaPI/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
+from diagnostico import diagnostico_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,6 +15,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('importacao/', include('apps.importacao.urls')),
+    path('_diagnostico/', diagnostico_view),
 ]
 
 if settings.DEBUG:
